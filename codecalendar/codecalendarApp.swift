6fde8327266b976caf50df9d2a27cd6de5b2fba2
Rebecca <becca.spasto@gmail.com>
2025-11-20 12:41:49 -0500

    Initial Commit

---
+++ codecalendar/codecalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  codecalendarApp.swift
+//  codecalendar
+//
+//  Created by Cam on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct codecalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
